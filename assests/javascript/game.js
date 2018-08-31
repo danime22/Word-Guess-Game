75e78cbf87eae42a6bf4bb859a54fb38a88967c5
Yan Davdison <yandavdison@Yans-MacBook-Pro.local>
2018-08-31 16:01:24 -0500

    game

--- assests/javascript/game.js
+++ assests/javascript/game.js
@@ -1,6 +1,38 @@
-$(document).ready(function(){
-    $("#btn").on("click", function(){
-        alert("HAVE FUN!!!");
-    })
-}
-)
+var anime = ["N", "A", "R", "U", "T", "O"];
+
+var Animes = [
+    {
+        title: "Naruto",
+        picture: "img",
+        themeSong: "song"
+    },
+    {
+        title: "onePiece",
+        picture: "img",
+        themeSong: "song"
+    },
+
+
+
+    {
+        title: "fairy tail",
+        picture: "img",
+        themeSong: "song"
+    },
+
+
+
+    {
+        title: "hunterXhunter",
+        picture: "img",
+        themeSong: "song"
+    },
+
+
+
+    {
+        title: "bleach",
+        picture: "img",
+        themeSong: "song"
+    }
+]
